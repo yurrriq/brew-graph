f0d7df823f96c95195ade644a7f453991e40179f
Eric Bailey <eric@ericb.me>
2017-01-01 21:06:00 -0600

    git rm script.hs

--- script.hs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env stack
-{- stack --resolver lts-7.14 --install-ghc runghc
-         --nix --nix-packages
-         "haskellPackages.graphviz haskellPackages.optparse-applicative haskellPackages.process"
--}
